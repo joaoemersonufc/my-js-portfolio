4eb937b22d9bfbc359b1aa2a8e956bf3be82b1fa
joaoemersonufc <joaoemersonufc@gmail.com>
2024-01-18 19:13:04 -0300

    fix: site details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
       <span className={styles.date}>
-        {`© ${new Date().getFullYear()} João Emerson. Inspired by Hamish Williams`}
+        {`© ${new Date().getFullYear()} João Emerson`}
       </span>
     </Text>
   </footer>
